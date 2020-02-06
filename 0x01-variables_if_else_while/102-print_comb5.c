70215e095726238ba169dd5efb9f403b306b054c
AlejoCasti <acastiblanco@gmail.com>
2020-02-06 20:37:16 +0000

    Update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+  int i, j, z, y;
+  for (i = 0; i < 10; i++)
+    {
+      for(j = 0; j < 10; j++)
+	{
+	  for (z = 0; z < 10; z++)
+	    {
+	      for(y = j+1; y < 10; y++)
+		{
+		  if( y >= j+1 || z >= i+1)
+		    {
+		      putchar('0' + i);
+		      putchar('0' + j);
+		      putchar(' ');
+		      putchar('0' + z);
+		      putchar('0' + y);
+		      if (i != 9 || j != 8 || z != 9 || y != 9)
+			{
+			  putchar(',');
+			  putchar(' ');
+			}
+		    }
+
+		}
+	    }
+
+	}
+    }
+  putchar('\n');
+  return 0;
+}
