dff3092860ba2c7027d206afa0eeb77e92074402
AlejoCasti <acastiblanco@gmail.com>
2020-02-06 19:12:07 +0000

    Update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,25 +7,24 @@
  */
 int main(void)
 {
-  int i, j, a;
-  for (i = 0; i < 10; i++)
-    {
-      a = i+1;
-      for(j = 0; j <10; j++)
-	{
-	  if( j >= a)
-	    {
-	      putchar('0' + i);
-	      putchar('0' + j);
-	      if(i != 8 || j != 9)
-		{
-		  putchar(',');
-		  putchar(' ');
+int i, j, a;
+for (i = 0; i < 10; i++)
+{
+a = i + 1;
+for (j = 0; j < 10; j++)
+{
+if (j >= a)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
 }
-	    }
-
-	}
-    }
-  putchar('\n');
-  return (0);
+}
+}
+}
+putchar('\n');
+return (0);
 }
