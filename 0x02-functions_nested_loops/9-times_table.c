b501a347a88957792e7e2754fecd6118d0f26f7e
AlejoCasti <acastiblanco@gmail.com>
2020-02-11 00:22:54 +0000

    Update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for(a = 0; a < 10; a ++)
+	{
+		for(b = 0; b < 10; b++)
+		{
+			c = b*a;
+			if (c > 9)
+			{
+				if (b != 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				_putchar('0' + c/10 );
+				_putchar('0' + c%10 );
+
+			}
+			else
+			{
+				if (b != 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar('0' + c);
+			}
+
+		}
+		_putchar('\n');
+	}       
+}
