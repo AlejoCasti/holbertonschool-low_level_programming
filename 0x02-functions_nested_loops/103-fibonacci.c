b0ce9730046c6ffd02b91c84ff48bfe6cb30d167
AlejoCasti <acastiblanco@gmail.com>
2020-02-11 19:47:02 +0000

    Update

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+ * imprimir - check the code for Holberton School students.
+ * @n: NUmber of value
+ * Return: Always 0.
+ */
+void imprimir(long n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		imprimir(n / 10);
+	_putchar(n % 10 + '0');
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	long n1 = 0, n2 = 1, temp;
+
+	for (i = 1; n2 <= 4000000; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			temp = n2;
+			n2 = n1 + n2;
+			n1 = temp;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
