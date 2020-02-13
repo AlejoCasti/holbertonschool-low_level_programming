4769bc580daadcebe0d687208765ee467fd7db25
AlejoCasti <acastiblanco@gmail.com>
2020-02-13 19:14:17 +0000

    Update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * primos - check the code for Holberton School students.
+ * @a: Number of value
+ * Return: If the number is prime return 1 if the number is not prime return 0.
  */
 int primos(unsigned long a)
 {
 	unsigned long int b;
-	for (b = 2; b != a; b--)
+	for (b = 2; b != a; b++)
 	{
 		if (a % b == 0)
 			return(0);
 	}
 	return(1);
 }
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	unsigned long size = 612852475143, a, b, primo;
-	for (a = size-1; a != 0; a--)
+	unsigned long size = 612852475143, cont, numA = size;
+	for (cont = 3; cont != size-1; cont++)
 	{
-		if (primos(a) == 1)
+		while (numA % cont == 0)
 		{
-			printf("Es primo %lu", a);
-			break;
+			if (primos(cont) == 1)
+			{
+				numA = numA / cont;
+				if (numA == 0)
+				{
+					printf("%lu", cont);
+				}
+			}
+				
 		}
+		
 	}
+	return(0);
 }
