4091a8e75336250db92c338678cfef655d13446b
AlejoCasti <acastiblanco@gmail.com>
2020-02-19 05:22:12 +0000

    upgrade the range of password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,24 +10,16 @@
 
 int main(void)
 {
-	int i, r, sum;
-	char array[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	char password[100];
+	int r, sum = 0;
 
 	srand(time(NULL));
-	sum = 0;
-	i = 0;
 	while (sum < (2772 - 122))
 	{
-		r = rand() % 62;
-		password[i] = array[r];
-		sum = sum + password[i];
-		i++;
+		r = rand() % (127 + 1 - 33) + 33;
+		putchar(r);
+		sum += r;
 	}
 	r = 2772 - sum;
-	password[i] = r;
-	i++;
-	password[i] = '\0';
-	printf("%s", password);
+	putchar(r);
 	return (0);
 }
