25ab924d1db8083be2435589090a8f3f3f9f1d25
AlejoCasti <acastiblanco@gmail.com>
2020-02-20 18:14:30 +0000

    Update!

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,12 +6,17 @@
  */
 char *cap_string(char *a)
 {
-	int i = 0;
+	int i = 0, car = 0;
 
 	while (a[i] != '\0')
 	{
-		if (!(a[i] >= 97 && a[i] <= 122))
+		if (a[i] >= 97 && a[i] <= 122)
 		{
+			if (car == 1)
+			{
+				a[i] -= 32;
+				car = 0;
+			}
 			i++;
 			continue;
 		}
@@ -24,10 +29,8 @@ char *cap_string(char *a)
 		    a[i - 1] == '(' ||
 		    a[i - 1] == ')' ||
 		    a[i - 1] == '{' ||
-		    a[i - 1] == '}' ||
-		    a[i - 1] == '\n' ||
-		    a[i - 1] == ' ')
-			a[i] -= 32;
+		    a[i - 1] == '}')
+			car = 1;
 		i++;
 	}
 	return (a);
