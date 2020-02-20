19f52d296ae8015a12f49eb4826196773d112bb8
AlejoCasti <acastiblanco@gmail.com>
2020-02-20 19:39:21 +0000

    Update

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * rot13 - check the code for Holberton School students.
+ * @a: array pointer
+ * Return: Always 0.
+ */
+char *rot13(char *a)
+{
+	char dic[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char leet[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j = 0;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		j = 0;
+		while (dic[j] != '\0')
+		{
+			if (a[i] == dic[j])
+				a[i] = leet[j];
+			j++;
+		}
+	}
+	return (a);
+}
