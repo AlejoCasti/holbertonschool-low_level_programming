d37ac8acb808c46148eb7471dbd94aed2fa72f63
AlejoCasti <acastiblanco@gmail.com>
2020-02-24 18:53:41 +0000

    Update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,18 +1,27 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
- * print_chessborad - prints buffer in hexa
+ * print_diagsums - prints buffer in hexa
  * @a: the address of memory to print
+ * @size: as
  * Return: Nothing.
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	unsigned int i = 0, j = 0;
-	for (i = 0; i < 8; i++)
+	int i, j, sum1 = 0, sum2 = 0, contador = 0;
+
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < 8; j++)
+		for (j = 0; j < size; j++)
 		{
-			_putchar((a[i])[j]);
+			if (j == i)
+				sum1 += a[contador];
+			if (j + i == size - 1)
+				sum2 += a[contador];
+			contador++;
 		}
-		_putchar('\n');
 	}
+	printf("%d, ", sum1);
+	printf("%d", sum2);
+	putchar('\n');
 }
