5ded910df8b76c6b3d24de43a6170ef189bdda01
AlejoCasti <acastiblanco@gmail.com>
2020-02-26 18:07:22 +0000

    Update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,29 @@
 #include "holberton.h"
 /**
- * _puts_recursion - check the code for Holberton School students.
- * @s: Pionter to the string
+ * getRoot - check the code for Holberton School students.
+ * @n: Pionter to the string
+ * @m: Number
  * Return: Always 0.
  */
-int _sqrt_recursion(int n)
+int getRoot(int n, int m)
 {
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	else if (n % 2 == 0)
-		return (1 + _sqrt_recursion(n % 2));
+	else if (m * m == n)
+		return (m);
+	else if (m * m > n)
+		return (-1);
 	else
-		return((n % 2) + 1);
+		return (getRoot(n, m + 1));
+}
+/**
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: Pionter to the string
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+	return (getRoot(n, 1));
 }
