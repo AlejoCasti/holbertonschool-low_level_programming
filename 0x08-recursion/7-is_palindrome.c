424976db35eac34e3cb081263b7adb1e81d9e432
AlejoCasti <acastiblanco@gmail.com>
2020-02-26 19:19:21 +0000

    Update

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: Pionter to the string
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+/**
+ * getPal - check the code for Holberton School students.
+ * @s: Pionter to the string
+ * @size: size
+ * Return: Always 0.
+ */
+int getPal(char *s, int size)
+{
+	if (*s != '\0')
+	{
+		if (*s == *(s + size))
+		{
+			getPal(s + 1, size - 1);
+		}
+		else
+			return (0);
+	}
+	return (1);
+}
+/**
+ * is_palindrome - check the code for Holberton School students.
+ * @s: Pionter to the string
+ * Return: Always 0.
+ */
+int is_palindrome(char *s)
+{
+	return (getPal(s, (_strlen_recursion(s) - 1)));
+}
