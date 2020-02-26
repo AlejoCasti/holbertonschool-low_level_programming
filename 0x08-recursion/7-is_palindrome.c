626a9d39f463bbe49ee62e07c65910e48ef51c77
AlejoCasti <acastiblanco@gmail.com>
2020-02-26 19:45:13 +0000

    Update

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -38,5 +38,10 @@ int getPal(char *s, int size)
  */
 int is_palindrome(char *s)
 {
-	return (getPal(s, (_strlen_recursion(s) - 1)));
+	int i = _strlen_recursion(s) - 1;
+
+	if (i <= 0)
+		return (1);
+	else
+		return (getPal(s, i));
 }
