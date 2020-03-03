63867c1235a08e670cb38d7ca1b5ef2d31c8c79e
AlejoCasti <acastiblanco8@gmail.com>
2020-03-03 17:24:00 +0000

    Fix an error the 0 task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,17 +11,11 @@ char *create_array(unsigned int size, char c)
 	if (size != 0)
 	{
 		char *p = malloc(size * sizeof(char));
+		unsigned int i;
 
-		if (*p)
-		{
-			unsigned int i;
-
-			for (i = 0; i < size; i++)
-				p[i] = c;
-			return (p);
-		}
-		else
-			return (NULL);
+		for (i = 0; i < size; i++)
+			p[i] = c;
+		return (p);
 	}
 	else
 		return (NULL);
