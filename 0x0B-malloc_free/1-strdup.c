6da79165135085532497bf8298bb9ab685e03855
AlejoCasti <acastiblanco8@gmail.com>
2020-03-03 17:32:00 +0000

    Push the numer one task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * str_len - documentation
+ * @s: pointer
+ * Return: the lenght of the array
+ */
+int str_len(char *s)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+/**
+ * _strdup - documentation
+ * @str: size of the array
+ * Return: the new pointer
+ */
+char *_strdup(char *str)
+{
+	if (!str)
+		return (NULL);
+	int size = str_len(str);
+	char *p = malloc(size * sizeof(char));
+	int i;
+
+	for (i = 0; i < size; i++)
+		p[i] = str[i];
+	return (p);
+}
