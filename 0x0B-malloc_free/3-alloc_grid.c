664af722f5d32782d740a9f9624b5263141652b3
AlejoCasti <acastiblanco8@gmail.com>
2020-03-03 23:56:29 +0000

    Fix 3 task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 		int **p = malloc(sizeof(*p) * width);
 		int i, j;
 
-		if (c == NULL || p == NULL)
+		if (!c || !p)
 			return (NULL);
 		for (i = 0; i < width; i++)
 		{
