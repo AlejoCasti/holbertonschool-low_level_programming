7fef1885ceb75ffb71faa4659b34e397a9555802
AlejoCasti <acastiblanco8@gmail.com>
2020-03-03 23:16:53 +0000

    Push 3 task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - documentation
+ * @width: width
+ * @height: height
+ * Return: the new pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	if (width > 0 && height > 0)
+	{
+		int *c = malloc(width * height * sizeof(int));
+		int i, j;
+
+		for (i = 0; i < width; i++)
+		{
+			for (j = 0; j < height; j++)
+				c[i * height + j] = 0;
+		}
+		int **p = malloc(sizeof(*p) * width);
+
+		for (i = 0; i < width; i++)
+			p[i] = c + i * height;
+		return (p);
+	}
+	else
+		return (NULL);
+}
