7bc1b936146da0817f68713bd3ed6b9808de805b
AlejoCasti <acastiblanco8@gmail.com>
2020-03-04 02:46:43 +0000

    push 5 task

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+/**
+ * str_len - documentation
+ * @s: pointer
+ * Return: the lenght of the array
+ */
+int str_len(char *s)
+{
+	if (*s)
+		return (1 + str_len(s + 1));
+	return (0);
+}
+/**
+ * argstostr - documentation
+ * @ac: array
+ * @av: array
+ * Return: the new pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	char *p;
+	int i, j, tam = 0, total = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 1; i < ac; i++)
+		tam += str_len(av[i]);
+	p = malloc(sizeof(char) * tam + ac);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			p[total] = av[i][j];
+			total++;
+		}
+		p[total++] = '\n';
+	}
+	return (p);
+}
