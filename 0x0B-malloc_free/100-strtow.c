3e19e507daa9c6ba991ae7808311a3fab459b1cc
AlejoCasti <acastiblanco8@gmail.com>
2020-03-04 03:55:43 +0000

    push 100 task

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+/**
+ * numWords - cont number of words
+ * @str: string
+ * Return: number of the word
+ */
+int numWords(char *str)
+{
+	int contador = 0;
+
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			if (*(str - 1) == ' ')
+				contador++;
+		}
+		str++;
+	}
+	return (contador);
+}
+/**
+ * strtow - split the string
+ * @str: string to split
+ * Return: A matrix split of string
+ */
+char **strtow(char *str)
+{
+	char **p;
+	int numWord = numWords(str), i = 0, j = 0;
+
+	p = malloc(sizeof(char *) * numWord + 1);
+	if (!p)
+		return (NULL);
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			if (*(str - 1) == ' ')
+			{
+				j = 0;
+				while (str[j] != ' ')
+					j++;
+				p[i] = malloc(sizeof(char) * j + 1);
+				if (!p[i])
+					return (0);
+				j = 0;
+				while (*str != ' ')
+				{
+					p[i][j] = *str;
+					j++;
+					str++;
+				}
+				p[i][j] = '\0';
+				i++;
+			}
+		}
+		str++;
+	}
+	p[i] = NULL;
+	return (p);
+}
