231efe8506210d1fa8a3c4046a9567aaa86f7c19
AlejoCasti <acastiblanco8@gmail.com>
2020-03-05 16:59:57 +0000

    Push 100 task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * array_range - create an array of integers
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new size
+ * Return: the pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (!ptr)
+			return (NULL);
+		return (ptr);
+	}	
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size > old_size)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
