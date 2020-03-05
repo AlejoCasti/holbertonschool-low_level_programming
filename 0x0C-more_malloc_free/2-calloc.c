950fc9312819debb854576b19126bf998e28f2ba
AlejoCasti <acastiblanco8@gmail.com>
2020-03-05 17:26:00 +0000

    Fix error 2 task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,11 +14,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (!p)
-	{
-		free(p);
 		return (NULL);
-	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		p[i] = 0;
 	return (p);
 }
