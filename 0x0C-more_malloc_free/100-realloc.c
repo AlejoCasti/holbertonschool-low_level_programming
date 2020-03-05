296e03d599fde2b1841bacd1c708d23fdb3d1610
AlejoCasti <acastiblanco8@gmail.com>
2020-03-05 17:07:10 +0000

    Fix error 100 task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,11 +8,14 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (ptr == NULL)
+	if (!ptr)
 	{
 		ptr = malloc(new_size);
 		if (!ptr)
+		{
+			free(ptr);
 			return (NULL);
+		}
 		return (ptr);
 	}
 	if (new_size == old_size)
@@ -27,6 +30,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	ptr = malloc(new_size);
 	return (ptr);
 }
