2b400bd093c9be9350f38f6c56b9c05fcf0a7de4
AlejoCasti <acastiblanco8@gmail.com>
2020-03-09 17:25:07 +0000

    Fix issue 4 task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,37 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+/**
+ * _strdup - check the code for Holberton School students.
+ * @str: pointer
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	int c = 0, s = 0;
+
+	char *d;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[s] != '\0')
+	{
+		s++;
+		c++;
+	}
+	c = 0;
+	d = malloc(sizeof(char) * (s + 1));
+	if (d == NULL)
+		return (NULL);
+	while (c < (s + 1))
+	{
+		d[c] = str[c];
+		c++;
+	}
+
+	if (str == NULL)
+		return (NULL);
+	return (d);
+}
 /**
  * new_dog - Initilize the values of variables to new dog
  * @name: poiinter to the name of dog
@@ -11,12 +42,16 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *newperrito;
+	char *namecpy;
+	char *ownercpy;
 
+	ownercpy = _strdup(owner);
+	namecpy = _strdup(name);
 	newperrito = malloc(sizeof(struct dog));
 	if (!newperrito)
 		return (NULL);
-	newperrito->name = strdup(name);
+	newperrito->name = namecpy;
 	newperrito->age = age;
-	newperrito->owner = strdup(owner);
+	newperrito->owner = ownercpy;
 	return (newperrito);
 }
