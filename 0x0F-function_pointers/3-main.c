6803bc1e9982281f2a3fa8bbb1b17955297cc535
AlejoCasti <acastiblanco8@gmail.com>
2020-03-11 19:11:41 +0000

    [Add] 3 task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - principal program
+ * @ac: numbers of arguments
+ * @ag: arguments
+ * Return: 0
+ */
+int main(int ac, char *ag[])
+{
+	int num1, num2, (*operate)(int, int);
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*ag[2] != '+' && *ag[2] != '-' && *ag[2] != '*' &&
+	    *ag[2] != '/' && *ag[2] != '%' && ag[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*ag[2] == '/' || *ag[2] == '%') && atoi(ag[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	num1 = atoi(ag[1]);
+	num2 = atoi(ag[3]);
+	operate = get_op_func(ag[2]);
+	printf("%d\n", operate(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - add two numbers
+ * @a: number to add
+ * @b: number to add
+ * Return: result of operate
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substraction two numbers
+ * @a: number to operate
+ * @b: number to operate
+ * Return: result of operate
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply two numbers
+ * @a: number to operate
+ * @b: number to operate
+ * Return: result of operate
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two numbers
+ * @a: number to operate
+ * @b: number to operate
+ * Return: result of operate
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - get mod two numbers
+ * @a: number to operate
+ * @b: number to operate
+ * Return: result of operate
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
