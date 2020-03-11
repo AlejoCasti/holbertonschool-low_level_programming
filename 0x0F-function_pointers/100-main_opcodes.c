5c58a9cf438ec5c74d690edb3cd6f47d0c76c411
AlejoCasti <acastiblanco8@gmail.com>
2020-03-11 23:50:30 +0000

    [Add] advanced task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - principal program
+ * @ac: numbers of args
+ * @ag: args
+ * Return: 0
+ */
+int main(int ac, char *ag[])
+{
+	unsigned char *mainptr = (unsigned char *) main;
+	int i;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(ag[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(ag[1]); i++)
+		if (i < atoi(ag[1]) - 1)
+			printf("%02X ", mainptr[i]);
+		else
+			printf("%02X\n", mainptr[i]);
+	return (0);
+}
