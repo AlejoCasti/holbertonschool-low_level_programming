7b04515c91cce91f25db6369edb30ea30b62b6f8
AlejoCasti <acastiblanco8@gmail.com>
2020-03-13 19:43:54 +0000

    Update repository

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,67 @@
+#include "variadic_functions.h"
+/**
+ * p_char - add two numbers
+ * @list: list of args
+ */
+void p_char(va_list list)
+{
+	printf("%c",  va_arg(list, int));
+}
+/**
+ * p_int - add two numbers
+ * @list: list of args
+ */
+void p_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
+/**
+ * p_string - add two numbers
+ * @list: list of args
+ */
+void p_string(va_list list)
+{
+	char *a =  va_arg(list, char *);
+
+	if (a == NULL)
+		a = "(nill)";
+	printf("%s", a);
+}
+/**
+ * p_float - add two numbers
+ * @list: list of args
+ */
+void p_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+/**
+ * print_all - print every arguments
+ * @format: format of string
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	va_list myList;
+	op_t ops[] = { {'c', p_char}, {'i', p_int}, {'f', p_float}, {'s', p_string}};
+	char *s = "";
+
+	va_start(myList, format);
+	while (format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == ops[j].op)
+			{
+				printf("%s", s);
+				ops[j].f(myList);
+				s = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(myList);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_DEF
+#define HOLBERTON_DEF
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char op;
+	void (*f)();
+} op_t;
+void print_all(const char * const format, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+#endif
