ed6a23ce4dd9d41856e1d9091eafccef023eed80
AlejoCasti <acastiblanco8@gmail.com>
2020-03-14 17:14:39 +0000

    [Fix] tasks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 	va_list myList;
-	
+
 	if (n != 0)
 	{
 		va_start(myList, n);
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,12 +10,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list myList;
 
-	if (separator)
+	va_start(myList, n);
+	for (i = 0; i < n - 1; i++)
 	{
-		va_start(myList, n);
-		for (i = 0; i < n - 1; i++)
+		if (separator)
 			printf("%d%s", va_arg(myList, int), separator);
-		printf("%d\n", va_arg(myList, int));
-		va_end(myList);
+		else
+			printf("%d", va_arg(myList, int));
 	}
+	printf("%d\n", va_arg(myList, int));
+	va_end(myList);
 }
