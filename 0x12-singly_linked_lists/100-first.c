1837e26fb4a7f17527544af45f14b93395654c56
AlejoCasti <acastiblanco8@gmail.com>
2020-03-20 03:50:11 +0000

    [FIX] advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-void constructor_fun(void) __attribute__ ((constructor));
-
 /**
  * constructor_fun - function that execute before main
  */
-void constructor_fun(void)
+void __attribute__ ((constructor)) constructor_fun(void)
 {
-	printf("You're beat! and yet, you must allow,
-\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
