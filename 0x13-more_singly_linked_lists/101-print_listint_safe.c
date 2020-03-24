7bce4615b6890e02c2e8ae40b97795ef9054aae7
AlejoCasti <acastiblanco8@gmail.com>
2020-03-24 15:34:30 +0000

    [ADD] advanced task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,21 +7,16 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	size_t i, j;
 	const listint_t *tmp, *tmp_t;
-	size_t i = 0, j = 0;
 
 	tmp = head;
 	for (i = 0; tmp; i++)
 	{
 		tmp_t = head;
-		j = 0;
-		while (i > j)
-		{
+		for (j = 0; i > j; j++, tmp_t = tmp_t->next)
 			if (tmp == tmp_t)
 				return (printf("-> [%p] %d\n", (void *)tmp, tmp->n), i);
-			j++;
-			tmp_t = tmp_t->next;
-		}
 		printf("[%p] %d\n", (void *)tmp, tmp->n);
 		tmp = tmp->next;
 	}
