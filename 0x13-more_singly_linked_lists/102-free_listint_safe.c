081e652bd4dc3d5c313202c40dfb32e69305d9df
AlejoCasti <acastiblanco8@gmail.com>
2020-03-24 17:50:13 +0000

    [FIX] Fifth try

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,18 +1,17 @@
 #include "lists.h"
-
 /**
- * free_listint_safe - free a list
+ * aux - find loop
  * @h: pointer to the list
  * Return: number of nodes
  */
-size_t free_listint_safe(listint_t **h)
+int aux(listint_t *h)
 {
-	listint_t *tmp = *h, *tmp_t;
-	size_t i, j, bool = 0, max = 0;
+	listint_t *tmp = h, *tmp_t;
+	int i, j, max = 0, bool = 0;
 
 	for (i = 0; tmp; i++, tmp = tmp->next)
 	{
-		tmp_t = *h;
+		tmp_t = h;
 		for (j = 0; i > j; j++, tmp_t = tmp_t->next)
 		{
 			if (tmp == tmp_t)
@@ -26,6 +25,19 @@ size_t free_listint_safe(listint_t **h)
 		if (bool == 1)
 			break;
 	}
+	return (max);
+}
+/**
+ * free_listint_safe - free a list
+ * @h: pointer to the list
+ * Return: number of nodes
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tmp;
+	size_t i, max;
+
+	max = aux(*h);
 	for (i = 0; i < max; i++)
 	{
 		tmp = *h;
