2ad8e50c99e7858361e36a38ac68af04bd00a434
AlejoCasti <acastiblanco8@gmail.com>
2020-03-24 17:51:29 +0000

    [ADD] advanced task

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,6 +16,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+size_t free_listint_safe(listint_t **h);
 size_t print_listint_safe(const listint_t *head);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint(const listint_t *h);
