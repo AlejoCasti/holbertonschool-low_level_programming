b2b4ac160cad1cf6decd352315a06d065a55c8b7
AlejoCasti <acastiblanco8@gmail.com>
2020-03-25 02:28:21 +0000

    [FIX] advaced task 103

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * find_listint_loop - find loop
+ * @head: pointer to the list
+ * Return: node that start loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tmp = head->next, *tmp_t = head->next->next;
+
+	while (tmp && tmp_t && tmp_t->next)
+	{
+		if (tmp == tmp_t)
+		{
+			tmp = head;
+			while (tmp != tmp_t)
+			{
+				tmp = tmp->next;
+				tmp_t = tmp_t->next;
+			}
+			return (tmp);
+		}
+		tmp = tmp->next;
+		tmp_t = tmp_t->next->next;
+	}
+	return (NULL);
+}
