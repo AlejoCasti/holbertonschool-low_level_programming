c1569bb55966eb5bf4de104e9d9123f762365f22
AlejoCasti <acastiblanco8@gmail.com>
2020-03-26 20:58:57 +0000

    [FIX] 4 task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,13 +8,13 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int c = (sizeof(unsigned long int) * 8) - 1;
-	int base = 1;
+	unsigned int base = 1;
 
 	if (index >= c)
 		return (-1);
 	while (index != 0)
 		base *= 2, index--;
-	if ((*n - base) > (base + *n))
+	if (*n < base)
 		*n = 0;
 	else
 		*n -= base;
