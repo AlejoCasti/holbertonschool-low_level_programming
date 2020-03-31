6ddb9307695caaa973545ff1f73d55a91a5e5af8
AlejoCasti <acastiblanco8@gmail.com>
2020-03-31 20:26:28 +0000

    [ADD-FIX] task 2, fix header file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * append_text_to_file - reads a text file and prints it
+ * @filename: file name
+ * @text_content: content of file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int ans, ans1 = 0;
+
+	ans = open(filename, O_RDWR | O_APPEND);
+	if (ans == -1)
+		return (-1);
+	if (!filename)
+		return (-1);
+	while (text_content[ans1])
+		ans1++;
+	if (write(ans, text_content, ans1) == -1)
+		return (close(ans), -1);
+	close(ans);
+	return (1);
+}
