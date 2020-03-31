2f1f36d8cb1e7c6720bbafbc9be5ad064c1f1739
AlejoCasti <acastiblanco8@gmail.com>
2020-03-31 20:02:17 +0000

    [FIX] return error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,8 @@ int create_file(const char *filename, char *text_content)
 	ans = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (ans == -1)
 		return (write(STDOUT_FILENO, "fails", 5), -1);
+	if (!text_content)
+		return (1);
 	if (!filename)
 		return (-1);
 	while (text_content[ans1])
