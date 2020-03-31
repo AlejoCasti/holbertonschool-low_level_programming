bf0d3de79760d8361e5632b4e7971cb904cad948
AlejoCasti <acastiblanco8@gmail.com>
2020-03-31 23:16:36 +0000

    [ADD] 3 taks

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * main - copy a file.
+ * @ac: number of args
+ * @av: args
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int ans, ans1, ans2 = 1, clo, clo1;
+	char buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "sage: cp file_from file_to\n");
+		exit(97);
+	}
+	ans = open(av[1], O_RDONLY);
+	if (ans < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	ans1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (ans1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while (ans2)
+	{
+		ans2 = read(ans, buffer, 1024);
+		if (ans2 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		else
+		{
+			if ((write(ans1, buffer, ans2)) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
+	}
+	clo = close(ans);
+	clo1 = close(ans1);
+	if (clo == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", clo), exit(100);
+	if (clo1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", clo1), exit(100);
+	return (0);
+}
