6c50ddf7480cf9d241bc90870ae772e41b10dc69
AlejoCasti <acastiblanco8@gmail.com>
2020-05-02 19:46:49 +0000

    [FIX] 8 task

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp = *head;
 	unsigned int i;
 
-	if (!*head)
+	if (!*head || index < 0)
 		return (-1);
 	if (index == 0)
 	{
