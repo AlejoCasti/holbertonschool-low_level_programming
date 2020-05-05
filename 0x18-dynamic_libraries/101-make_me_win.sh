78a34d88f53a9fbd7fa6d1eb3b473ad03d36449a
AlejoCasti <acastiblanco8@gmail.com>
2020-05-05 02:18:18 +0000

    [FIX]

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/AlejoCasti/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libputs.so?raw=true
+wget -P /tmp https://github.com/AlejoCasti/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libputs.so
 export LD_PRELOAD=/tmp/libputs.so
