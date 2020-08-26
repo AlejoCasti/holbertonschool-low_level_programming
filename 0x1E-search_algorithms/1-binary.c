23039cd7dbd2ed629a1bc585066e771cc5f81973
AlejoCasti <acastiblanco8@gmail.com>
2020-08-26 19:23:59 +0000

    [ADD] project

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * re_bi - recursive function to binary search algorithm
+ * @Low: Lowest number
+ * @Hi: Highest number
+ * @array: array to search
+ * @value: value to search
+ * Return: index of founded value
+ */
+int re_bi(size_t Low, size_t Hi, int *array, int value)
+{
+	size_t mid, i;
+	int vmid;
+
+	if (Low == Hi)
+		return (-1);
+	mid = ((Hi - Low) / 2) + Low;
+	vmid = array[mid];
+
+	printf("Searching in array: ");
+	for (i = Low; i < Hi; i++)
+		printf("%i, ", array[i]);
+	printf("%i\n", array[Hi]);
+
+	if (vmid == value)
+		return (mid);
+	if (value > vmid)
+		return (re_bi(mid + 1, Hi, array, value));
+	if (value < vmid)
+		return (re_bi(Low, mid - 1, array, value));
+	return (-1);
+}
+
+/**
+ * binary_search - binary search algorithm
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search
+ * Return: index of founded value
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return (re_bi(0, size - 1, array, value));
+}
