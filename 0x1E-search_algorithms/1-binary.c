be217a32a05dda8ade9c4814be4459d1ea88e584
AlejoCasti <acastiblanco8@gmail.com>
2020-08-26 19:40:42 +0000

    [ADD] 1 task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,37 +1,5 @@
 #include <stdio.h>
 /**
- * re_bi - recursive function to binary search algorithm
- * @Low: Lowest number
- * @Hi: Highest number
- * @array: array to search
- * @value: value to search
- * Return: index of founded value
- */
-int re_bi(size_t Low, size_t Hi, int *array, int value)
-{
-	size_t mid, i;
-	int vmid;
-
-	printf("Searching in array: ");
-	for (i = Low; i < Hi; i++)
-		printf("%i, ", array[i]);
-	printf("%i\n", array[Hi]);
-
-	if (Low == Hi)
-		return (-1);
-	mid = ((Hi - Low) / 2) + Low;
-	vmid = array[mid];
-
-	if (vmid == value)
-		return (mid);
-	if (value > vmid)
-		return (re_bi(mid + 1, Hi, array, value));
-	if (value < vmid)
-		return (re_bi(Low, mid - 1, array, value));
-	return (-1);
-}
-
-/**
  * binary_search - binary search algorithm
  * @array: array to search
  * @size: size of array
@@ -40,7 +8,32 @@ int re_bi(size_t Low, size_t Hi, int *array, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t low = 0, hi = size - 1, mid, i;
+	int vmid;
+
 	if (!array)
 		return (-1);
-	return (re_bi(0, size - 1, array, value));
+
+	while (low <= hi)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < hi; i++)
+			printf("%i, ", array[i]);
+		printf("%i\n", array[hi]);
+
+		if (low == hi)
+			return (-1);
+		mid = (hi + low) / 2;
+		vmid = array[mid];
+
+		if (vmid == value)
+			return (mid);
+		if (value > vmid)
+			low = 1 + mid;
+		if (value < vmid)
+			hi = mid - 1;
+	}
+
+
+	return (-1);
 }
