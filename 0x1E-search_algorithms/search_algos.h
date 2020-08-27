51e865d0912bb62b6b646338d79e6681b6d90cd7
AlejoCasti <acastiblanco8@gmail.com>
2020-08-27 00:26:53 +0000

    [ADD] header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+#include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
